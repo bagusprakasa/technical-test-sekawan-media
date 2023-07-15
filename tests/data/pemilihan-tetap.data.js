3d5ede8675e40c53d6bec837bf2964d17dd718b1
bagusprakasa <47969692+bagusprakasa@users.noreply.github.com>
2023-07-15 09:53:06 +0700

    Setup : preparation project

---
+++ tests/data/pemilihan-tetap.data.js
@@ -0,0 +1,17 @@
+export const VALID_LOGIN = {
+  username: 'candidat',
+  password: "password123"
+};
+
+export const SEARCH_DATA = {
+  valid: 'aaaaa123',
+  invalid: 'qq',
+  blank: ' ',
+};
+
+export const FILE ={
+  valid: './tests/file/Data Pemilih 2023-07-14 22_32_58 Valid.xlsx',
+  invalid: './tests/file/Data Pemilih 2023-07-14 22_32_58 Tidak Valid.xlsx',
+  blank: './tests/file/Data Pemilih 2023-07-14 23_27_08 Blank.xlsx',
+  duplikat: './tests/file/Data Pemilih 2023-07-14 22_32_58 Duplikat.xlsx',
+}
---
+++ tests/helpers/elements.js
@@ -0,0 +1,23 @@
+export function get(selector) {
+  return cy.get(selector);
+}
+
+export function fillFilled(selector, value) {
+  return cy.get(selector).type(value).should("have.value", value);
+}
+
+export function fillFilledClear(selector, value) {
+  return cy.get(selector).clear().type(value).should("have.value", value);
+}
+
+export function fillSelect(selector, value) {
+  return cy.get(selector).select(value).should("have.value", value);
+}
+
+export function fillFile(selector, value) {
+  return cy.get(selector).selectFile(value);
+}
+
+export function click(selector, ...args) {
+  return cy.get(selector).click(...args);
+}
